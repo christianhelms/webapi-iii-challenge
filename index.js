e2ee5b6020459c796d10d246ecf875efa9961130
christianhelms <helmsmedia@gmail.com>
2019-08-07 17:36:58 -0500

    :rocket: api setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code away!
+const express = require('express');
+
+const postsRouter = require('./posts/postRouter.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+server.listen(8000, () => console.log('\nAPI running\n'));
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,42 @@
-const express = 'express';
+const router = require("express").Router();
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The posts could not be retrieved." });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  const postId = req.params.id;
+  posts
+    .findById(postId)
+    .first()
+    .then(post => {
+      if (!post) {
+        return res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-
-};
+function validatePostId(req, res, next) {}
 
 module.exports = router;
